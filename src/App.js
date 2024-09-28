698a6c53cf0d001e65209f18a7bf8dca96da3a9b
Abhishek Shaw <abhishekshaw2k20@gmail.com>
2024-09-28 12:08:15 +0530

    Changes done

---
+++ src/App.js
@@ -0,0 +1,60 @@
+//src/App.tsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import HowItWorks from "./components/HowItWorks";
+import BookEnquiry from "./components/BookEnquiry";
+import MissionAndVision from "./components/MissionAndVision";
+import RefundPolicy from "./components/RefundPolicy";
+import Quote from "./components/Quote";
+import CancelBooking from "./components/CancelBooking";
+import FAQS from "./components/FAQS";
+import TermsAndCondition from "./components/TermsAndCondition";
+import AppAppBar from "./components/AppAppBar";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import ProtectedComponent from "./components/ProtectedComponent";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
+import ForgotPassword from "./components/ForgotPassword";
+import WebDashboard from "./components/WebDashboard";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <AppAppBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/book-enquiry" element={<BookEnquiry />} />
+          <Route path="/mission-and-vision" element={<MissionAndVision />} />
+          <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="/cancel-booking" element={<CancelBooking />} />
+          <Route path="/faq" element={<FAQS />} />
+          <Route path="/terms-conditions" element={<TermsAndCondition />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route
+            path="/protected-route"
+            element={
+              <PrivateRoute>
+                <ProtectedComponent />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<WebDashboard />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
